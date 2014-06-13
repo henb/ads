1bbb11edad30daa00ddd44e04bf8256c846369fe
henb <henb2015@gmail.com>
2014-06-13 17:33:47 +0300

    Создание static_page контроллера для статических страниц

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ads::Application.routes.draw do
+  get "static_page/home"
+  get "static_page/about"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
