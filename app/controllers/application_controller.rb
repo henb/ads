5878a8bd8e86c5bbdcde67658022994a033d563f
henb <henb2015@gmail.com>
2014-06-13 17:31:43 +0300

    Добавление Имени и Фамилии к devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   protect_from_forgery with: :exception
+
+  protected  
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u| 
+    	u.permit( :first_name,
+    		      :last_name,
+    		      :email,
+    		      :password,
+    		      :password_confirmation,
+    		      :reset_password_token,
+    		      :reset_password_sent_at,
+    		      :remember_created_at) 
+      end
+    end
 end
