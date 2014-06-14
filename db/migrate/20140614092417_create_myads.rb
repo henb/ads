906115093119154587d1bc2c3bcd96fb4c363ee5
henb <henb2015@gmail.com>
2014-06-14 20:08:54 +0300

    добавление поля state

--- db/migrate/20140614092417_create_myads.rb
+++ db/migrate/20140614092417_create_myads.rb
@@ -1,9 +1,14 @@
 class CreateMyads < ActiveRecord::Migration
   def change
     create_table :myads do |t|
-      t.string :title
-      t.text :description
-      t.integer :typead_id
+      t.string :title, null: false, default: ""
+      t.text :description, null: false, default: ""
+      t.integer :typead_id, null: false
+
+      #state_machine
+      t.integer :state
+      t.index :state
+      t.text :admin_message
 
       t.timestamps
     end
