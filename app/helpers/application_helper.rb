a41a562a611ac7e87b89a69205e33b822ea8b6d8
henb <henb2015@gmail.com>
2014-06-17 09:04:39 +0300

    Добавлено flash сообщение

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,5 +17,8 @@ module ApplicationHelper
   def states_ad
     Myad.state_machine.states.map &:name
   end
-
+  
+  def flash_key(key)
+    ['info','warning','danger','success','default'].include?(key.to_s) ? key : 'info'  
+  end
 end
