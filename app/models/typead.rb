d8d5472bea02219e2bf3e49c8dea857b202b5b71
henb <henb2015@gmail.com>
2014-06-20 15:19:46 +0300

    Улучшение вида type_ad

--- app/models/typead.rb
+++ app/models/typead.rb
@@ -1,6 +1,6 @@
 class Typead < ActiveRecord::Base
 	has_many :myads
-
+    default_scope -> { order('name ASC') }
 	validates :name, presence: true, length: { in:10..100 }
     validates :description, presence: true, length: { maximum:500 }
 end
