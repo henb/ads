a8761ff45ca326f80120bbc0b36ffbd421121dca
henb <henb2015@gmail.com>
2014-06-22 17:01:23 +0300

    Написаны тесты для User модели

---
+++ spec/factories/user.rb
@@ -0,0 +1,26 @@
+require "faker"
+
+FactoryGirl.define do
+
+  factory :user do
+    first_name Faker::Name.first_name
+    last_name  Faker::Name.last_name
+    sequence(:email) { |n| "test#{n}.#{Faker::Internet.safe_email}" }
+
+    password '11111111'
+    password_confirmation '11111111'
+ 
+    factory :guest_user do
+      role 'guest'
+    end
+
+    factory :user_user do
+      role 'user'
+    end
+
+    factory :admin_user do
+      role 'admin'
+    end
+ 
+  end
+end
