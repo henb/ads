cd51f7666f6df9331cd3073a40e17f1cf5f3bc5a
henb <henb2015@gmail.com>
2014-07-05 16:11:14 +0300

    Замена turbolinks на jquery.turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jquery_nested_form
 //= require bootstrap-wysihtml5
-//= require turbolinks
+//= require jquery.turbolinks
 //= require bootstrap
 //= require_tree .
-
