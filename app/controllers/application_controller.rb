d13389b23b1a520873998c1d4bedfc1032da5d92
henb <henb2015@gmail.com>
2014-07-12 21:34:18 +0300

    Удалены неиспользуемые комментарии

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-  # before_filter do
-  #   resource = controller_name.singularize.to_sym
-  #   method = "#{resource}_params"
-  #   params[resource] &&= send(method) if respond_to?(method, true)
-  # end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_action :configure_permitted_parameters, if: :devise_controller?
