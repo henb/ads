f55ae873de73eac0589dc1055864884926c5186f
henb <henb2015@gmail.com>
2014-07-12 10:29:12 +0300

    Написаны routing тесты

---
+++ spec/routing/typeads_routing_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe TypeadsController do
+  describe "resources" do
+    it "#index" do
+      expect(get: "/typeads").to route_to(
+          controller: "typeads", action: "index")
+    end
+
+    it "#new" do
+      expect(get: "/typeads/new").to route_to(
+          controller: "typeads", action: "new")
+    end
+
+    it "#show" do
+      expect(get: "/typeads/1").to route_to(
+          controller: "typeads", action: "show", id: "1")
+    end
+
+    it "#edit" do
+      expect(get: "/typeads/1/edit").not_to route_to(
+          controller: "typeads", action: "edit", id: "1")
+    end
+
+    it "#create" do
+      expect(post:"/typeads").to route_to(
+          controller: "typeads", action: "create")
+    end
+
+    it "#update" do
+      expect(put: "/typeads/1").not_to route_to(
+          controller: "typeads", action: "update", id: "1")
+    end
+
+    it "#destroy" do
+      expect(delete: "/typeads/1").to route_to(
+          controller: "typeads", action: "destroy", id: "1")
+    end
+  end
+end
---
+++ spec/routing/users_routing_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe UsersController do
+  describe "resources" do
+    it "#index" do
+      expect(get: "/users").to route_to(
+          controller: "users", action: "index")
+    end
+
+    it "#show" do
+      expect(get: "/users/1").to route_to(
+          controller: "users", action: "show", id: "1")
+    end
+
+    it "#edit" do
+      expect(get: "/users/1/edit").to route_to(
+          controller: "users", action: "edit", id: "1")
+    end
+
+    it "#update" do
+      expect(put: "/users/1").to route_to(
+          controller: "users", action: "update", id: "1")
+    end
+
+    it "#new" do
+      expect(get: "/users/new").not_to route_to(
+          controller: "users", action: "new")
+    end
+
+    it "#create" do
+      expect(post: "/users").not_to route_to(
+          controller: "users", action: "create")
+    end
+
+    it "#destroy" do
+      expect(delete: "/users/1").to route_to(
+          controller: "users", action: "destroy", id: "1")
+    end
+  end
+end
