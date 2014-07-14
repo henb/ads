f4200698313a5d88af30e6050e338a6044e39a82
henb <henb2015@gmail.com>
2014-07-14 10:52:59 +0300

    Удалён default_scope из Myad

--- app/models/myad.rb
+++ app/models/myad.rb
@@ -4,8 +4,6 @@ class Myad < ActiveRecord::Base
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, reject_if: ->(image) { image[:url].blank? }, allow_destroy: true
 
-  default_scope -> { order('updated_at DESC') }
-
   validates :typead_id, presence: true
   validates :title, presence: true, length: { in: 10..100 }
   validates :description, presence: true, length: { maximum: 500 }
