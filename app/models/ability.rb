4ed2d25dc2013a52ebe039f1f7bce9b84cf096ff
henb <henb2015@gmail.com>
2014-07-14 20:29:37 +0300

    Улучшен Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,20 +4,21 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    can :read, Typead
-    can :read, Myad, state_name: :published, user_id: user.id
-    cannot :index, Myad
-    can :published, Myad
+    alias_action *Myad.state_machine.events.map(&:name), to: :events
+    #alias_action *Myad.state_machine.events.map(&:name), to: :update_all_state
 
+    can :read, Typead
+    can :read, Myad, state: 4 # state_name: :published
+    can :published, Myad
+    can :fresh, Myad
     if user.user?
-      can :read, [User, Myad]
+      can :read, User
+      can :read, Myad, user_id: user.id
       can :create, Myad
-      can :update, Myad, state_name: :drafting, user_id: user.id
+      can [:edit, :update], Myad, state_name: :drafting
       can :destroy, Myad, user_id: user.id
-      cannot :destroy, Myad do |ad|
-        ad.state_name.in? [:banned, :rejected]
-      end
-      can :event, Myad do |ad|
+      cannot :destroy, Myad, state_name: [:banned, :rejected]
+      can :events, Myad do |ad|
         !(ad.state_events & Myad.user_events).empty?
       end
       can :update_all_state, Myad
@@ -26,10 +27,8 @@ class Ability
       can :manage, [User, Typead]
       can :destroy, :all
       cannot [:create, :update], Myad
-      can :read, Myad do |ad|
-        ad.state_name.in? [:freshing, :approved, :banned, :rejected]
-      end
-      can :event, Myad do |ad|
+      can :read, Myad, state: Myad.admin_state
+      can :events, Myad do |ad|
         !(ad.state_events & Myad.admin_events).empty?
       end
       can :update_all_state, Myad
