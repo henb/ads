b6008dd425a52d2970a90a81de9d5172af4bafb4
henb <henb2015@gmail.com>
2014-07-16 18:31:29 +0300

    Пересмотр хелперов

--- app/helpers/myads_helper.rb
+++ app/helpers/myads_helper.rb
@@ -1,4 +1,34 @@
 module MyadsHelper
+  def label_state(index)
+    arr = [:default, :primary, :warning, :info, :success, :archive, :danger]
+    return :none unless (0...arr.size).include? index
+    arr[index]
+  end
+
+  def can_event?(ad)
+    !(ad.state_events & (admin? ? Myad.admin_events : Myad.user_events)).empty?
+  end
+
+  def label_event(symbol)
+    label_state events_ad.index(symbol)
+  end
+
+  def events_ad
+    Myad.state_machine.events.map &:name
+  end
+
+  def events_ad_for_select_tag
+    events_ad & (admin? ? Myad.admin_events : Myad.user_events)
+  end
+
+  def states_ad
+    Myad.state_machine.states.map &:name
+  end
+
+  def states_hash
+    states_ad.each_with_index.map { |e, i| [e, i] }.to_h
+  end
+
   def admin_states_ad
     states_ad - [:drafting, :archives]
   end
