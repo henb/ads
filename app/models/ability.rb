4b1e36e96f051e91e9ef5db9ea07bd646c91c8ad
henb <henb2015@gmail.com>
2014-07-16 18:42:35 +0300

    Улучшен ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    alias_action *Myad.state_machine.events.map(&:name), to: :events
+    alias_action *Myad.user_events,  to: :user_events
+    alias_action *Myad.admin_events, to: :admin_events
     # alias_action *Myad.state_machine.events.map(&:name), to: :update_all_state
     can :read, Typead
     can :read, Myad, state: 4 # state_name: :published
@@ -15,19 +16,15 @@ class Ability
       can :update, Myad, state_name: :drafting
       can :destroy, Myad, user_id: user.id
       cannot :destroy, Myad, state_name: [:banned, :rejected]
-      can :events, Myad do |ad|
-        !(ad.state_events & Myad.user_events).empty?
-      end
+      can :user_events, Myad
       can :update_all_state, Myad
-    elsif user.admin?
 
+    elsif user.admin?
       can :manage, [User, Typead]
       can :destroy, :all
       cannot [:create, :update], Myad
       can :read, Myad, state: Myad.admin_state
-      can :events, Myad do |ad|
-        !(ad.state_events & Myad.admin_events).empty?
-      end
+      can :admin_events, Myad
       can :update_all_state, Myad
     end
   end
